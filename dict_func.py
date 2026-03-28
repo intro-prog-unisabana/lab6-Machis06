cb59c9eda5645859b13f158b920c77fc5c550036
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-28 17:30:20 -0500

    lab 6

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,16 @@
-# Write your code here!
+def employee_print(employee_info):
+    data = employee_info.copy()
+    name = data.pop("Name", "N/A")
+    salary = data.pop("Salary", "N/A")
+    role = data.pop("Role", "N/A")
+   
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+    if data:
+        for key, value in data.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
+
+    
