0f93caffd95a1effb54dcd5d5fe0f1ccffa57b2f
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-28 18:47:50 -0500

    lab 6

--- overall.py
+++ overall.py
@@ -1,28 +1,19 @@
-def student_averages(students):
-    result = {}
-    
-    for student, grades in students.items():
-        avg = sum(grades.values()) / len(grades)
-        result[student] = round(avg)
-    
-    return result
-
-
 def assignment_averages(students):
     result = {}
-    
-    # Obtener nombres de tareas (del primer estudiante)
-    assignments = next(iter(students.values())).keys()
-    
-    for assignment in assignments:
+
+    if not students:
+        return result
+
+    first_student = list(students.values())[0]
+
+    for assignment in first_student:
         total = 0
         count = 0
-        
+
         for grades in students.values():
             total += grades[assignment]
             count += 1
-        
-        avg = total / count
-        result[assignment] = round(avg)
-    
+
+        result[assignment] = round(total / count)
+
     return result
