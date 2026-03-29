5cc9135ec1df5ac4b3651814e1d41cb6504e00e3
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-28 20:35:37 -0500

    lab 6

--- industrial_temperature.py
+++ industrial_temperature.py
@@ -1,8 +1,2 @@
 def trigger_alarm(temperatures, threshold=80):
-    alarms = []
-
-    for sensor in temperatures:
-        if temperatures[sensor] > threshold:
-            alarms.append(sensor)
-
-    return alarms
+    return [sensor for sensor, temp in temperatures.items() if temp > threshold]
